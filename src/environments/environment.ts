b5c2cafec246b505fc359fa1a05b6f20f8147522
Cristian Flores <humbertflores57@gmail.com>
2025-11-19 10:29:19 -0400

    version 1.1.21

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api/'
+  apiUrl: 'http://http://192.168.120.249:4200//api/'
+  // apiUrl: 'http://localhost:8000/api/'
   // apiUrl: '/api/'
 };
